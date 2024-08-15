e44fdabddf6825df4465371476955c9b032d4a58
junstory <lastcom1@gmail.com>
2024-08-15 20:18:35 +0900

    Next/TS Study #3

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # blog
 Personal Bolg 
+
+db.json은 StudyNext 폴더에서 사용되는 파일입니다.
--- StudyNext/src/app/page.tsx
+++ StudyNext/src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
    <>
    <h2>Welcome!</h2>
    Hello, Next js!
+   <br></br><img src="/next.svg"></img>
    </>   
   );
 }
