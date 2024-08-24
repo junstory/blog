9d0b292c3991f9fddb9db713ab59393e9f9476e9
junstory <lastcom1@gmail.com>
2024-08-25 01:52:27 +0900

    Feat. mdx 파일로 글작성하기

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+// };
+
+// export default nextConfig;
+
+
+import createMDX from '@next/mdx'
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-};
-
-export default nextConfig;
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+ 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
